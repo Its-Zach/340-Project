93eb8390d356fdd90c9e5e71051410dc9ac9604e
Jesse Torres <jessejtorres1987@gmail.com>
2025-12-11 23:04:03 -0700

    port actually fix

--- server.js
+++ server.js
@@ -139,7 +139,7 @@ app.get('/', (req, res) => {
   res.send('One Piece IoT API is running');
 });
 
-const PORT = process.env.PORT || 10000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
