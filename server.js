e8702ad731a4d389a8322ed178a5b355843e8c20
Jesse Torres <jessejtorres1987@gmail.com>
2025-12-12 02:58:50 -0700

    alexa

--- server.js
+++ server.js
@@ -138,6 +138,22 @@ app.get("/latestReading", async (req, res) => {
   }
 });
 
+app.post('/alexa', (req, res) => {
+    console.log('Alexa request received:', req.body);
+
+    // Minimal response to test skill
+    res.json({
+        version: "1.0",
+        response: {
+            outputSpeech: {
+                type: "PlainText",
+                text: "Hello from your Alexa skill!"
+            },
+            shouldEndSession: true
+        }
+    });
+});
+
 // 4️⃣ UPDATE (edit island_id + character_id)
 app.put("/updateReading/:id", async (req, res) => {
   const reading_id = Number(req.params.id);
